cb1847dbc73cfc21ebc317e1fc6c3a49b0abfa1e
VicenteAGT <155429830+VicenteAGT@users.noreply.github.com>
2025-11-11 05:15:12 +0000

    [Updates by vicenteagt]

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -7,10 +7,24 @@ import { TrafficLight } from "./TrafficLight.jsx";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center mt-4">
-            <TrafficLight/>
+		<div className="container-fluid">
+         <TrafficLight/>
 		</div>
 	);
 };
 
 export default Home;
+
+//    <div className="row trafficLightContainer">
+// 				<div className="col-6 trafficLightUpper m-auto mt-4">
+// 					<div className="d-flex flex-column mt-3">
+// 					<button type="button" class="btn btn-danger redLightContainer"></button>
+// 					<button type="button" class="btn btn-warning yellowLightContainer mt-1"></button>
+// 					<button type="button" class="btn btn-success greenLightContainer mt-1"></button>
+// 					</div>
+// 				</div>
+// 			</div>
+// 			<div className="row">
+// 				<div className="col-1 trafficLightLower m-auto">
+// 				</div>
+// 			</div>
--- src/js/components/TrafficLight.jsx
+++ src/js/components/TrafficLight.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
 export const TrafficLight = () => {
-    let [redLight, setRedLight] = useState("primary");
-    let [yellowLight, setYellowLight] = useState("primary");
-    let [greenLight, setGreenLight] = useState("primary");
+    let [redLight, setRedLight] = useState("withoutLightContainerHook");
+    let [yellowLight, setYellowLight] = useState("withoutLightContainerHook");
+    let [greenLight, setGreenLight] = useState("withoutLightContainerHook");
 
     // let cambiarcolor = () => {
     //     if (redLight === "primary") {
@@ -16,31 +16,38 @@ export const TrafficLight = () => {
     //         setGreenLight("danger")
     // }
     let cambiarcolor = () => {
- setRedLight("danger")
- setYellowLight("primary")
-setGreenLight("primary")
+ setRedLight("redLightContainerHook")
+ setYellowLight("withoutLightContainerHook")
+setGreenLight("withoutLightContainerHook")
     }
     let cambiarcolor2 = () => {
-setRedLight("primary")
- setYellowLight("warning")
-setGreenLight("primary")
+setRedLight("withoutLightContainerHook")
+ setYellowLight("yellowLightContainerHook")
+setGreenLight("withoutLightContainerHook")
     }
      let cambiarcolor3 = () => {
- setRedLight("primary")
- setYellowLight("primary")
-setGreenLight("success")
+ setRedLight("withoutLightContainerHook")
+ setYellowLight("withoutLightContainerHook")
+setGreenLight("greenLightContainerHook")
     }
 
 
     return (
-        <div className="d-flex flex-column m-auto">
-            <div className="row">
-            <button type="button" className={"btn " + "btn-" + redLight} onClick={cambiarcolor}>A</button>
-            <button type="button" className={"btn " + "btn-" + yellowLight} onClick={cambiarcolor2}>B</button>
-            <button type="button" className={"btn " + "btn-" + greenLight} onClick={cambiarcolor3}>C</button>
-        
-        </div>
-</div>
+        <div>
+ <div className="row trafficLightContainer">
+				<div className="col-6 trafficLightUpper m-auto mt-4">
+					<div className="d-flex flex-column mt-3">
+					<button type="button" class={"btn btn-danger redLightContainer " + redLight} onClick={cambiarcolor}></button>
+					<button type="button" class={"btn btn-warning yellowLightContainer mt-1 " + yellowLight} onClick={cambiarcolor2}></button>
+					<button type="button" class={"btn btn-success greenLightContainer  mt-1 " + greenLight} onClick={cambiarcolor3}></button>
+					</div>
+				</div>
+			</div>
+			<div className="row">
+				<div className="col-1 trafficLightLower m-auto">
+				</div>
+			</div>
+            </div>
     )
 
 }
@@ -58,3 +65,13 @@ setGreenLight("success")
     // } else if (lightPressed === 'green') {
     // )
     //     setGreenLight("success"); 
+
+
+    //         <div className="d-flex flex-column m-auto">
+//             <div className="row">
+//             <button type="button" className={"btn " + "btn-" + redLight} onClick={cambiarcolor}>A</button>
+//             <button type="button" className={"btn " + "btn-" + yellowLight} onClick={cambiarcolor2}>B</button>
+//             <button type="button" className={"btn " + "btn-" + greenLight} onClick={cambiarcolor3}>C</button>
+        
+//         </div>
+// </div>
